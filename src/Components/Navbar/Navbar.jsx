26c0d3b003b7011b14ba0550e948d828a49575c4
pawan <bpawan277@gmail.com>
2023-12-06 13:04:43 +0530

    letgo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
             style={{
               float: "left",
               fontSize: "30px",
-              padding: "13px 10px 13px 10px",
+              padding: "9px 10px 13px 9px",
               fontWeight: "600",
             }}
           >
