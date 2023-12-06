b2949889d64c73661453aa7a4a4c411a6109d7ba
pawan <bpawan277@gmail.com>
2023-12-06 13:12:33 +0530

    changecontentasd

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
             style={{
               float: "left",
               fontSize: "30px",
-              padding: "9px 10px 13px 9px",
+              padding: "9px 10px 9px 9px",
               fontWeight: "600",
             }}
           >
