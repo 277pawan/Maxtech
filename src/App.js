03d76b2e633832e61ae5cee735da95079283d7b7
pawan <bpawan277@gmail.com>
2023-12-07 18:08:58 +0530

    surelanding

--- src/App.js
+++ src/App.js
@@ -8,24 +8,35 @@ import Subscribe from './Components/Subscribe/Subscribe';
 import Footer from './Components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Default from './Components/Default/Default';
+import Landing from './Components/Landingpage/Landing';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [landing, setlanding] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setlanding(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [])
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<>
-            <Header />
-            <Course />
-            <Blog />
-            <Guide />
-            <Subscribe />
-            <Footer />
-          </>}></Route>
-          <Route path='*' element={<Default />}></Route>
-        </Routes>
-      </BrowserRouter>
+      {landing ? (<Landing />) :
+        (<BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<>
+              <Header />
+              <Course />
+              <Blog />
+              <Guide />
+              <Subscribe />
+              <Footer />
+            </>}></Route>
+            <Route path='*' element={<Default />}></Route>
+          </Routes>
+        </BrowserRouter>
+        )}
     </div>
   );
 }
---
+++ src/Components/Landingpage/Landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Landing.css";
+function Landing() {
+  return (
+    <div className="landingcontainer">
+      <div>Maxtech..</div>
+    </div>
+  );
+}
+
+export default Landing;
