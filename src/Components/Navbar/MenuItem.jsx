c4760252fe3de9da06d33a364571546e78b6221b
pawan <bpawan277@gmail.com>
2024-09-30 23:12:43 +0530

    adding animated navbar

---
+++ src/Components/Navbar/MenuItem.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#9FA0FF", "#4400FF"];
+const name = ["Home", "Project", "About", "Contact", "Coffee"];
+export const MenuItem = ({ i }) => {
+  const style = { border: `2px solid ${colors[i]}` };
+  return (
+    <motion.li
+      variants={variants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <div className="icon-placeholder" style={style}></div>
+      <div className="text-placeholder">{name[i]}</div>
+    </motion.li>
+  );
+};
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { motion, useCycle } from "framer-motion";
+import { useDimensions } from "./Dimmensions";
+import { Navigation } from "./Navigation";
+import { MenuToggle } from "./Menutoogle";
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 function Navbar() {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
+
   return (
     <>
       <div className="navbarcontainer">
@@ -35,6 +63,17 @@ function Navbar() {
           </Link>
         </ul>
       </div>
+
+      <motion.nav
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        custom={height}
+        ref={containerRef}
+      >
+        <motion.div className="background" variants={sidebar} />
+        <Navigation />
+        <MenuToggle toggle={() => toggleOpen()} />
+      </motion.nav>
     </>
   );
 }
